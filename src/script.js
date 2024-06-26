30da88e0e3e71f42116bb6706a3bb5b1cf72dbf8
rugs07 <rugwed07@gmail.com>
2024-06-27 00:02:40 +0530

    Lets connect commit

--- src/script.js
+++ src/script.js
@@ -2,7 +2,9 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-
+import {FontLoader} from 'three/examples/jsm/loaders/FontLoader.js'
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry.js'
+import { BufferGeometry } from 'three'
 // Debug
 const gui = new dat.GUI()
 
@@ -13,24 +15,78 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const axeshelper = new THREE.AxesHelper();
+scene.add(axeshelper);  
+// const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const textureloader = new THREE.TextureLoader()
+const matcaps = textureloader.load('/textures/matcaps/1.jpg')
+const matcapss = textureloader.load('/textures/matcaps/3.jpg')
+const fontloader = new THREE.FontLoader()
 
+fontloader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>{
+        const textgeometry = new THREE.TextBufferGeometry(
+            'Lets Connect',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 5,
+                bevelEnabled: true,
+                bevelThickness:0.03,
+                bevelSize:0.02,
+                bevelOffset:0,
+                bevelSegments:4
+                
+            }
+        )
+        textgeometry.computeBoundingBox()
+        textgeometry.translate(
+            textgeometry.boundingBox.max.x * 0.005 - 2,
+            textgeometry.boundingBox.max.y * 0.05 - 0.25 ,
+            textgeometry.boundingBox.max.z * 0.05 - 0.45
+
+        )
+        // textgeometry.center();
+
+        const textmaterial = new THREE.MeshMatcapMaterial({matcap : matcaps})
+        const text = new THREE.Mesh(textgeometry,textmaterial)
+        scene.add(text);
+
+        for(let i =0;i <100;i++){
+            const donutgeometry = new THREE.TorusGeometry(0.3,0.2,20,45)
+            const donutmaterial = new THREE.MeshMatcapMaterial({matcap : matcapss})
+            const donut = new THREE.Mesh(donutgeometry,donutmaterial)
+
+            donut.position.x = (Math.random()-0.5) * 10
+            donut.position.y = (Math.random()-0.5) * 10
+            donut.position.z = (Math.random()-0.5) * 10
+
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            const scale = Math.random()
+            donut.scale.set(scale,scale,scale)
+            scene.add(donut)
+        }
+    }
+)
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// const material = new THREE.MeshBasicMaterial()
+// material.color = new THREE.Color(0xff0000)
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// const sphere = new THREE.Mesh(geometry,material)
+// scene.add(sphere)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+// const pointLight = new THREE.PointLight(0xffffff, 0.1)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
+// scene.add(pointLight)
 
 /**
  * Sizes
@@ -65,8 +121,7 @@ camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
+const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
 
 /**
@@ -90,7 +145,7 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    // sphere.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
